6f5ad3150f12d6716d43341775ae264d8d4bb11e
PrasanaVenkateshS <prasana.seshadri@gmail.com>
2024-02-19 23:22:05 -0700

    fix readLine

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const WebSocket = require("ws");
 const { v4: uuidv4 } = require("uuid");
 const uuid = uuidv4();
 const SERVER_URL = "ws://192.168.1.154:8080/";
-const MY_SYSTEM = "Shek";
+const MY_SYSTEM = "Prasana";
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,40 +15,46 @@ const socket = new WebSocket(SERVER_URL);
 socket.addEventListener("open", function (m) {});
 
 socket.addEventListener("message", function (m) {
-    const parsedMessages = JSON.parse(m.data); 
-    // Filter out non-JSON strings
-    const jsonMessages = parsedMessages.filter(message => {
-        try {
-            JSON.parse(message);
-            return true;
-        } catch (error) {
-            return false;
-        }
-    });
+  const parsedMessages = JSON.parse(m.data);
+  // Filter out non-JSON strings
+  const jsonMessages = parsedMessages.filter((message) => {
+    try {
+      JSON.parse(message);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  });
 
-    // Parse JSON strings as objects and process them
-    jsonMessages.forEach(function(message) {
-        const parsedMessage = JSON.parse(message);
+  // Parse JSON strings as objects and process them
+  jsonMessages.forEach(function (message) {
+    const parsedMessage = JSON.parse(message);
 
-        if (parsedMessage && parsedMessage.id && parsedMessage.name && parsedMessage.msg) {
-            // Skip messages sent by the current client
-            if (parsedMessage.id !== uuid) {
-                // Display message details
-                console.log(`${parsedMessage.name}: ${parsedMessage.msg}`);
-            }
-        }
-    });
+    if (
+      parsedMessage &&
+      parsedMessage.id &&
+      parsedMessage.name &&
+      parsedMessage.msg
+    ) {
+      // Skip messages sent by the current client
+      if (parsedMessage.id !== uuid) {
+        // Display message details
+        console.log(`${parsedMessage.name}: ${parsedMessage.msg}`);
+      }
+    }
+  });
 });
 
 // // Prompt user for input
 function promptUser() {
-  rl.question((message) => {
+  rl.question("", (message) => {
     var obj = new Object();
     obj.id = uuid;
     obj.name = MY_SYSTEM;
     obj.msg = message;
-
-    socket.send(JSON.stringify(obj));
+    if (message == null || message == " ") {
+      socket.send(JSON.stringify(obj));
+    }
     setTimeout(() => promptUser(), 500);
   });
 }
