1564a2990cf212f5c0ba6e796d95a1d512181e14
Abhishek-Ram25 <aramak21@asu.edu>
2024-02-19 21:34:51 -0700

    add eventListener for response

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const readline = require("readline");
 const WebSocket = require("ws");
 const { v4: uuidv4 } = require("uuid");
-const id = uuidv4();
+const uuid = uuidv4();
 const SERVER_URL = "ws://192.168.1.154:8080/";
 
 const rl = readline.createInterface({
@@ -13,7 +13,19 @@ const socket = new WebSocket(URL);
 
 socket.addEventListener("open", function (m) {});
 
-socket.addEventListener("message", function (m) {});
+socket.addEventListener("message", function (m) {
+    const parsedMessages = JSON.parse(m.data); 
+    parsedMessages.forEach(function(message) {
+        
+        if (message && message.id && message.name && message.message) {
+            // Skip messages sent by the current client
+            if (message.id !== uuid) {
+                // Display message details
+                console.log(`ID: ${message.id}, Name: ${message.name}, Message: ${message.message}`);
+            }
+        }
+    });
+});
 
 // // Prompt user for input
 function promptUser() {
